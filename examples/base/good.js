10bb2fc8b50912dc5550bb3b229cca1dd259a33c
ryym <ryym.64@gmail.com>
2016-11-08 14:43:41 +0900

    Add basic example

---
+++ examples/base/good.js
@@ -0,0 +1,12 @@
+function fizzBuzz(n) {
+  if (n % 15) {
+    return "FizzBuzz"
+  } else if (n % 3) {
+    return "Fizz"
+  } else if (n % 5) {
+    return "Buzz"
+  }
+  return ""
+}
+
+module.exports = fizzBuzz
