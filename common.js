a677a6a507db8ade3df52adf070587a00b1801ae
ybiquitous <10koba01@gmail.com>
2017-07-12 16:00:42 +0900

    refactor: remove duplicated `parserOptions`
    The `parserOptions` entry is already defined in `eslint-config-airbnb-base`.
    See https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/index.js#L11
    
    The difference is only below:
    
    ```
    - "ecmaVersion": 6,
    + "ecmaVersion": 2017,
    ```
    
    The below command is utility in order to show entire ESLint configuration:
    
    ```
    ./node_modules/.bin/eslint --print-config common.js
    ```

--- common.js
+++ common.js
@@ -1,12 +1,4 @@
 module.exports = {
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-    },
-  },
-
   "plugins": [
     "eslint-comments",
   ],
